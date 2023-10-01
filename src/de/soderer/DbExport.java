639c5b87783c2889b0abcf38cf1cab808aedd0ae
HuDeanY <github@soderer.de>
2023-10-01 23:48:00 +0200

    Fixed errorneous checkin of dbexport

--- src/de/soderer/DbExport.java
+++
@@ -1,9 +0,0 @@
-package de.soderer;
-
-import de.soderer.utilities.jarinjarloader.JarInJarLoader;
-
-public class DbExport extends JarInJarLoader {
-	public static void main(final String[] args) throws Exception {
-		jarInJarLoaderStart(args);
-	}
-}
